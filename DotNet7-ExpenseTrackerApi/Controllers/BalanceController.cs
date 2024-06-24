8b66172ed2420936913512d159f6021a7b3dc15a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-25 02:02:41 +0630

    Format Code

--- DotNet7-ExpenseTrackerApi/Controllers/BalanceController.cs
+++ DotNet7-ExpenseTrackerApi/Controllers/BalanceController.cs
@@ -1,14 +1,16 @@
-﻿using DotNet7_ExpenseTrackerApi.Models.Entities;
+﻿using System.Data.SqlClient;
+using DotNet7_ExpenseTrackerApi.Models.Entities;
 using DotNet7_ExpenseTrackerApi.Queries;
 using DotNet7_ExpenseTrackerApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
 
 namespace DotNet7_ExpenseTrackerApi.Controllers;
+
 public class BalanceController : BaseController
 {
     private readonly IConfiguration _configuration;
     private readonly AdoDotNetService _adoDotNetService;
+
     public BalanceController(IConfiguration configuration, AdoDotNetService adoDotNetService)
     {
         _configuration = configuration;
@@ -23,7 +25,10 @@ public class BalanceController : BaseController
         {
             string query = BalanceQuery.GetBalanceList();
             List<SqlParameter> parameters = new();
-            List<BalanceModel> lst = _adoDotNetService.Query<BalanceModel>(query, parameters.ToArray());
+            List<BalanceModel> lst = _adoDotNetService.Query<BalanceModel>(
+                query,
+                parameters.ToArray()
+            );
 
             return Content(lst);
         }
