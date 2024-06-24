6b720efa19435515b93c1c29d6a2cf68555a87c4
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-25 02:06:00 +0630

    Format Code

--- DotNet7-ExpenseTrackerApi/Services/JwtService.cs
+++ DotNet7-ExpenseTrackerApi/Services/JwtService.cs
@@ -1,10 +1,11 @@
-﻿using DotNet7_ExpenseTrackerApi.Models.Entities;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using DotNet7_ExpenseTrackerApi.Models.Entities;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DotNet7_ExpenseTrackerApi.Services;
+
 public class JwtService
 {
     private readonly IConfiguration _configuration;
@@ -18,18 +19,21 @@ public class JwtService
 
     public string GenerateJWTToken(UserModel user)
     {
-        List<Claim> claims = new()
-        {
-            new Claim("UserId", _aesService.EncryptString(user.UserId.ToString())),
-            new Claim("UserName", _aesService.EncryptString(user.UserName)),
-            new Claim("Email", _aesService.EncryptString(user.Email)),
-            new Claim("UserRole", _aesService.EncryptString(Convert.ToString(user.UserRole)))
-        };
+        List<Claim> claims =
+            new()
+            {
+                new Claim("UserId", _aesService.EncryptString(user.UserId.ToString())),
+                new Claim("UserName", _aesService.EncryptString(user.UserName)),
+                new Claim("Email", _aesService.EncryptString(user.Email)),
+                new Claim("UserRole", _aesService.EncryptString(Convert.ToString(user.UserRole)))
+            };
 
         string keyString = _configuration["Jwt:Key"]!;
         if (string.IsNullOrEmpty(keyString) || keyString.Length < 32)
         {
-            throw new ArgumentException("The JWT key is not valid. Ensure it is at least 32 characters long.");
+            throw new ArgumentException(
+                "The JWT key is not valid. Ensure it is at least 32 characters long."
+            );
         }
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
@@ -39,7 +43,8 @@ public class JwtService
             _configuration["Jwt:Audience"],
             claims,
             expires: DateTime.UtcNow.AddMonths(1),
-            signingCredentials: signIn);
+            signingCredentials: signIn
+        );
 
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
