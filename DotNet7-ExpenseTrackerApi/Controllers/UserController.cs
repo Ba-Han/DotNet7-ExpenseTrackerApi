a73cf86f9d20b82fac39654e33d427c579b44387
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-25 02:04:07 +0630

    Format Code

--- DotNet7-ExpenseTrackerApi/Controllers/UserController.cs
+++ DotNet7-ExpenseTrackerApi/Controllers/UserController.cs
@@ -1,11 +1,11 @@
-﻿using DotNet7_ExpenseTrackerApi.Enums;
+﻿using System.Data;
+using System.Data.SqlClient;
+using DotNet7_ExpenseTrackerApi.Enums;
 using DotNet7_ExpenseTrackerApi.Models.Entities;
 using DotNet7_ExpenseTrackerApi.Models.RequestModels.User;
 using DotNet7_ExpenseTrackerApi.Queries;
 using DotNet7_ExpenseTrackerApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace DotNet7_ExpenseTrackerApi.Controllers;
 
@@ -14,7 +14,12 @@ public class UserController : BaseController
     private readonly AdoDotNetService _adoDotNetService;
     private readonly IConfiguration _configuration;
     private readonly JwtService _jwtService;
-    public UserController(AdoDotNetService service, IConfiguration configuration, JwtService jwtService)
+
+    public UserController(
+        AdoDotNetService service,
+        IConfiguration configuration,
+        JwtService jwtService
+    )
     {
         _adoDotNetService = service;
         _configuration = configuration;
@@ -47,42 +52,41 @@ public class UserController : BaseController
 
             if (requestModel.Gender == EnumGender.Male.ToString())
                 requestModel.Gender = EnumGender.Male.ToString();
-
             else if (requestModel.Gender == EnumGender.Female.ToString())
                 requestModel.Gender = EnumGender.Female.ToString();
-
             else if (requestModel.Gender == EnumGender.Other.ToString())
                 requestModel.Gender = EnumGender.Other.ToString();
-
             else
                 return BadRequest("Invalid Gender");
 
             string duplicateQuery = UserQuery.GetDuplicateEmailQuery();
-            List<SqlParameter> duplicateParams = new()
-            {
-                new SqlParameter("@Email", requestModel.Email),
-                new SqlParameter("@IsActive", true)
-            };
-            DataTable user = _adoDotNetService.QueryFirstOrDefault(duplicateQuery, duplicateParams.ToArray());
+            List<SqlParameter> duplicateParams =
+                new()
+                {
+                    new SqlParameter("@Email", requestModel.Email),
+                    new SqlParameter("@IsActive", true)
+                };
+            DataTable user = _adoDotNetService.QueryFirstOrDefault(
+                duplicateQuery,
+                duplicateParams.ToArray()
+            );
             if (user.Rows.Count > 0)
             {
                 return Conflict("User with this email already exists. Please login.");
             }
             string query = UserQuery.CreateRegisterQuery();
-            SqlCommand cmd = new(query, conn)
-            {
-                Transaction = transaction
-            };
-            List<SqlParameter> parameters = new()
-            {
-                new SqlParameter("@UserName", requestModel.UserName),
-                new SqlParameter("@Email", requestModel.Email),
-                new SqlParameter("@Password", requestModel.Password),
-                new SqlParameter("@UserRole", requestModel.UserRole),
-                new SqlParameter("@DOB", requestModel.DOB),
-                new SqlParameter("@Gender", requestModel.Gender),
-                new SqlParameter("@IsActive", true)
-            };
+            SqlCommand cmd = new(query, conn) { Transaction = transaction };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new SqlParameter("@UserName", requestModel.UserName),
+                    new SqlParameter("@Email", requestModel.Email),
+                    new SqlParameter("@Password", requestModel.Password),
+                    new SqlParameter("@UserRole", requestModel.UserRole),
+                    new SqlParameter("@DOB", requestModel.DOB),
+                    new SqlParameter("@Gender", requestModel.Gender),
+                    new SqlParameter("@IsActive", true)
+                };
             cmd.Parameters.AddRange(parameters.ToArray());
             long userID = Convert.ToInt64(cmd.ExecuteScalar());
 
@@ -90,16 +94,14 @@ public class UserController : BaseController
             if (userID != 0)
             {
                 string balanceQuery = BalanceQuery.CreateBalanceQuery();
-                List<SqlParameter> balanceParams = new()
-                {
-                    new SqlParameter("@UserId", userID),
-                    new SqlParameter("@Amount", 0),
-                    new SqlParameter("@CreateDate", DateTime.Now)
-                };
-                SqlCommand balanceCmd = new(balanceQuery, conn)
-                {
-                    Transaction = transaction
-                };
+                List<SqlParameter> balanceParams =
+                    new()
+                    {
+                        new SqlParameter("@UserId", userID),
+                        new SqlParameter("@Amount", 0),
+                        new SqlParameter("@CreateDate", DateTime.Now)
+                    };
+                SqlCommand balanceCmd = new(balanceQuery, conn) { Transaction = transaction };
 
                 balanceCmd.Parameters.AddWithValue("@UserId", userID);
                 balanceCmd.Parameters.AddWithValue("@Amount", 0);
@@ -150,46 +152,50 @@ public class UserController : BaseController
 
             if (requestModel.Gender == EnumGender.Male.ToString())
                 requestModel.Gender = EnumGender.Male.ToString();
-
             else if (requestModel.Gender == EnumGender.Female.ToString())
                 requestModel.Gender = EnumGender.Female.ToString();
-
             else if (requestModel.Gender == EnumGender.Other.ToString())
                 requestModel.Gender = EnumGender.Other.ToString();
-
             else
                 return BadRequest("Invalid Gender");
 
             string duplicateQuery = UserQuery.CheckUserEixstsQuery();
-            List<SqlParameter> duplicateParams = new()
-            {
-                new SqlParameter("@UserName", requestModel.UserName),
-                new SqlParameter("@Email", requestModel.Email),
-                new SqlParameter("@UserRole", requestModel.UserRole),
-                new SqlParameter("@DOB", requestModel.DOB),
-                new SqlParameter("@Gender", requestModel.Gender),
-                new SqlParameter("@IsActive", true),
-                new SqlParameter("@UserId", id)
-            };
-            DataTable dt = _adoDotNetService.QueryFirstOrDefault(duplicateQuery, duplicateParams.ToArray());
+            List<SqlParameter> duplicateParams =
+                new()
+                {
+                    new SqlParameter("@UserName", requestModel.UserName),
+                    new SqlParameter("@Email", requestModel.Email),
+                    new SqlParameter("@UserRole", requestModel.UserRole),
+                    new SqlParameter("@DOB", requestModel.DOB),
+                    new SqlParameter("@Gender", requestModel.Gender),
+                    new SqlParameter("@IsActive", true),
+                    new SqlParameter("@UserId", id)
+                };
+            DataTable dt = _adoDotNetService.QueryFirstOrDefault(
+                duplicateQuery,
+                duplicateParams.ToArray()
+            );
             if (dt.Rows.Count > 0)
                 return Conflict("User Name already exists.");
 
             //updateRegisterQuery
             string query = UserQuery.UpdateRegisterQuery();
-            List<SqlParameter> parameters = new()
-            {
-                new SqlParameter("@UserName", requestModel.UserName),
-                new SqlParameter("@Email", requestModel.Email),
-                new SqlParameter("@UserRole", requestModel.UserRole),
-                new SqlParameter("@DOB", requestModel.DOB),
-                new SqlParameter("@Gender", requestModel.Gender),
-                new SqlParameter("@IsActive", true),
-                new SqlParameter("@UserId", id)
-            };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new SqlParameter("@UserName", requestModel.UserName),
+                    new SqlParameter("@Email", requestModel.Email),
+                    new SqlParameter("@UserRole", requestModel.UserRole),
+                    new SqlParameter("@DOB", requestModel.DOB),
+                    new SqlParameter("@Gender", requestModel.Gender),
+                    new SqlParameter("@IsActive", true),
+                    new SqlParameter("@UserId", id)
+                };
             int result = _adoDotNetService.Execute(query, parameters.ToArray());
 
-            return result > 0 ? StatusCode(202, "Updating Successful!") : BadRequest("Updating Fail!");
+            return result > 0
+                ? StatusCode(202, "Updating Successful!")
+                : BadRequest("Updating Fail!");
         }
         catch (Exception ex)
         {
@@ -203,16 +209,21 @@ public class UserController : BaseController
     {
         try
         {
-            if (requestModel is null || string.IsNullOrEmpty(requestModel.Email) || string.IsNullOrEmpty(requestModel.Password))
+            if (
+                requestModel is null
+                || string.IsNullOrEmpty(requestModel.Email)
+                || string.IsNullOrEmpty(requestModel.Password)
+            )
                 return BadRequest("Email or Password is empty.");
 
             string query = UserQuery.GetLoginQuery();
-            List<SqlParameter> parameters = new()
-            {
-                new SqlParameter("@Email", requestModel.Email),
-                new SqlParameter("@Password", requestModel.Password),
-                new SqlParameter("@IsActive", true),
-            };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new SqlParameter("@Email", requestModel.Email),
+                    new SqlParameter("@Password", requestModel.Password),
+                    new SqlParameter("@IsActive", true),
+                };
 
             List<UserModel> lst = _adoDotNetService.Query<UserModel>(query, parameters.ToArray());
             if (lst is null)
@@ -220,7 +231,10 @@ public class UserController : BaseController
 
             UserModel userDataModel = lst[0];
 
-            return StatusCode(202, new { access_token = _jwtService.GenerateJWTToken(userDataModel) });
+            return StatusCode(
+                202,
+                new { access_token = _jwtService.GenerateJWTToken(userDataModel) }
+            );
         }
         catch (Exception ex)
         {
@@ -235,10 +249,7 @@ public class UserController : BaseController
         try
         {
             string query = UserQuery.GetLoginlistQuery();
-            List<SqlParameter> parameters = new()
-            {
-                new SqlParameter("@IsActive", true)
-            };
+            List<SqlParameter> parameters = new() { new SqlParameter("@IsActive", true) };
             List<UserModel> lst = _adoDotNetService.Query<UserModel>(query, parameters.ToArray());
 
             return Ok(lst);
