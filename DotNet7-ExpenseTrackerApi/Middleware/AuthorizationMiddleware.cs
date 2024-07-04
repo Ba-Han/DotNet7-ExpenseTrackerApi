8b2fe853e1b5a1efecaa3adc945d109fdb263c5c
Ba-Han <96304699+Ba-Han@users.noreply.github.com>
2024-07-04 15:24:26 +0630

    .

--- DotNet7-ExpenseTrackerApi/Middleware/AuthorizationMiddleware.cs
+++ DotNet7-ExpenseTrackerApi/Middleware/AuthorizationMiddleware.cs
@@ -3,70 +3,66 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
-namespace DotNet7_ExpenseTrackerApi.Middleware;
-
-public class AuthorizationMiddleware
+namespace DotNet7_ExpenseTrackerApi.Middleware
 {
-    private readonly RequestDelegate _requestDelegate;
-    private readonly IConfiguration _configuration;
-
-    public AuthorizationMiddleware(IConfiguration configuration, RequestDelegate requestDelegate)
+    public class AuthorizationMiddleware
     {
-        _configuration = configuration;
-        _requestDelegate = requestDelegate;
-    }
+        private readonly RequestDelegate _requestDelegate;
+        private readonly IConfiguration _configuration;
 
-    public async Task InvokeAsync(HttpContext context)
-    {
-        string? authHeader = context.Request.Headers["Api_Key"];
-        string requestPath = context.Request.Path;
-
-        if (requestPath == "api/account/register" || requestPath == "api/account/login")
+        public AuthorizationMiddleware(IConfiguration configuration, RequestDelegate requestDelegate)
         {
-            await _requestDelegate(context);
-            return;
+            _configuration = configuration;
+            _requestDelegate = requestDelegate;
         }
 
-        if (
-            authHeader != null
-            && !string.IsNullOrEmpty(authHeader)
-            && authHeader.StartsWith("Bearer")
-        )
+        public async Task InvokeAsync(HttpContext context)
         {
-            string[] hearder_and_token = authHeader.Split(' ');
-            string header = hearder_and_token[0];
-            string token = hearder_and_token[1];
-            string privateKey = _configuration.GetSection("EncryptionKey").Value!;
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-            byte[] key = Encoding.ASCII.GetBytes(privateKey);
+            string? authHeader = context.Request.Headers["Authorization"];
+            string requestPath = context.Request.Path;
 
-            TokenValidationParameters parameters = new TokenValidationParameters()
-            {
-                RequireExpirationTime = true,
-                ValidateIssuer = false,
-                ValidateAudience = true,
-                IssuerSigningKey = new SymmetricSecurityKey(key)
-            };
-
-            ClaimsPrincipal principal = tokenHandler.ValidateToken(
-                token,
-                parameters,
-                out SecurityToken securityToken
-            );
-
-            if (principal is not null)
+            if (requestPath == "/api/account/register" || requestPath == "/api/account/login")
             {
                 await _requestDelegate(context);
                 return;
             }
-            else
+
+            if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Bearer "))
             {
-                context.Response.StatusCode = 401;
-                return;
+                string token = authHeader.Substring("Bearer ".Length).Trim();
+                string privateKey = _configuration.GetSection("EncryptionKey").Value!;
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                byte[] key = Encoding.ASCII.GetBytes(privateKey);
+
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+
+                try
+                {
+                    ClaimsPrincipal principal = tokenHandler.ValidateToken(
+                        token,
+                        parameters,
+                        out SecurityToken securityToken
+                    );
+
+                    if (principal != null)
+                    {
+                        await _requestDelegate(context);
+                        return;
+                    }
+                }
+                catch (SecurityTokenException)
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
             }
-        }
-        else
-        {
+
             context.Response.StatusCode = 401;
             return;
         }
